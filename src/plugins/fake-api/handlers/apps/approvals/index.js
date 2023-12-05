e2a17e9b2c3ac88c3b777052865e5dfc5029555b
Gren CISSE Al-Karid <grencisse@hotmail.com>
2023-12-05 07:45:41 +0000

    Checkpoint 1

---
+++ src/plugins/fake-api/handlers/apps/approvals/index.js
@@ -0,0 +1,10 @@
+import { db } from '@db/apps/approvals/db'
+import { rest } from 'msw'
+
+export const handlerApprovalRequests = [
+  rest.get(('/api/apps/approvalrequests'), (req, res, ctx) => {
+    let approvalRequests = db.approvalRequests//.filter(request => (request.status.toLowerCase().includes("pending")))
+    
+    return res(ctx.status(200), ctx.json({ approvalRequests }))
+  }),
+]
