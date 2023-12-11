8ce9a153e7328e980c91a99264bb96cd8dd3d730
Gren CISSE Al-Karid <grencisse@hotmail.com>
2023-12-11 17:26:50 +0000

    Update Approvals App: arrange vertical menu

---
+++ src/navigation/vertical/apps.js
@@ -0,0 +1,16 @@
+export default [
+  { heading: 'Apps & Pages' },
+
+  {
+    title: 'Approvals',
+    icon: { icon: 'tabler-book' },
+    children: [
+      { title: 'A Valider', to: 'apps-approvals' },
+      { title: 'Historique', to: 'apps-approvals-history' },
+      { title: 'Soumettre', to: 'apps-approvals-submit' },
+
+      //{ title: 'View', to: { name: 'apps-approvals-view-id', params: { id: 10 } } },
+    ],
+  },
+]
+  
